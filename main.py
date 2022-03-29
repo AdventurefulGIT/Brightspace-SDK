472633a77ca718c3a63ed9b0915ca102a565e35b
Marc <marcandre2002@hotmail.com>
2022-03-29 20:57:28 -0300

    Generate Image

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import BrightspaceClient
+import datetime
+import imgkit
 import pickle
 import json
 import re
@@ -12,7 +14,8 @@ class BSManager:
             if not "/d2l/home" in self.session.get(url).url:
                 raise Exception("Dead session")
         except:
-            self.session = BrightspaceClient.Brightspace(url, TwoFactorAuthentication=input("On Campus?: ")).session
+            answer = True if input("Two Factor Required?(0/1): ") == 1 else False
+            self.session = BrightspaceClient.Brightspace(url, TwoFactorAuthentication=True).session
             with open('./session.bkl', 'wb') as f:
                 pickle.dump(self.session, f)
 
@@ -84,7 +87,7 @@ class BSManager:
         return re.findall(r"<title>Homepage - (.*)<\/title>", response.text)[0]
 
         
-    def get_course_assignments(self, course_id):
+    def get_course_assignments(self, course_id, due_check=True):
             response = self.session.request(
                 method = "GET",
                 url = f"{self.__url}/d2l/lms/dropbox/user/folders_list.d2l?ou={course_id}"
@@ -112,12 +115,47 @@ class BSManager:
                     assignment_date = assignment_date.replace('\u00a0', "Unknown")
                     
                     assignment = {"Name":assignment_name, "Due Date":assignment_date}
+
+                    if due_check:
+                        if assignment_date != "Unknown":
+                            if datetime.datetime.strptime(assignment_date, "%b %d, %Y %I:%M %p") <= datetime.datetime.now():
+                                continue
                     assignments[list(assignments.keys())[-1]].append(assignment)    
-            return json.dumps(assignments, indent=4, sort_keys=False)
+            return assignments#json.dumps(assignments, indent=4, sort_keys=False)
         
 
+    def generate_display(self, term, assignments):
+        page = f''' 
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+ 
+            <h1>{term}<h1>
+        '''
 
+        def add_class(course, course_content):
+            html = f"<h3 class='font-weight-bold'>{course}</h3>"
 
+            for value in course_content:
+                if len(course_content[value]) == 0:
+                    continue
+                html += f"<h4 class><u>{value}</u></h4>\n"
+
+                html += "<table class='table'><tr><th class='w-50 text-left'>Name</th><th class='w-50 text-left'>Due Date</th></tr>\n"
+                
+                for assign in course_content[value]:
+                    html += f"<tr><td>{assign['Name']}</td><td>{assign['Due Date']}</td></tr>"
+                html += "</table>"
+
+                
+
+            return html if len(html.split('\n')) != 1 else ""
+
+        for course in assignments:
+            page += add_class(course, assignments[course])
+
+        return imgkit.from_string(page, False, options={'width': 800, 'disable-smart-width': ''})
+        
 if __name__ == "__main__":
     url = input("Url: ")
     client = BSManager(url)
@@ -129,6 +167,12 @@ if __name__ == "__main__":
     semester = list(semesters.keys())[int(input("\nSelect a semester: "))-1]
 
     courses = client.get_courses(semesters[semester])
+    details = {}
     for course in courses:
-        print(client.get_course_name(course))
-        print(client.get_course_assignments(course))
+        details[client.get_course_name(course)] = client.get_course_assignments(course)
+
+    md = client.generate_display(semester, details)
+
+    #print(md)
+    #Html2Image().screenshot(md, save_as="out.png")
+    
